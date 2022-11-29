946345338cfbc9c6d96e92a9e9a51bd1734e80d4
Nathan Krasney <natankrasney@gmail.com>
2022-11-29 21:10:47 +0200

    test in loop

--- src/tests/utils.test.ts
+++ src/tests/utils.test.ts
@@ -2,7 +2,9 @@
 import {expect, test} from 'vitest';
 import { sum } from '../logic/utils';
 
-test('1+2 is 3' , ()=>{
-expect(sum(1,2)).toBe(3); 
+for (let index = 0; index < 2; index++) {
+    test('1+2 is 3' , ()=>{
+        expect(sum(1,2)).toBe(3); 
+        })    
+}
 
-})
