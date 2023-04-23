eaa611a634ee373e519ae1140674f19fe4ed131d
Nathan Krasney <natankrasney@gmail.com>
2023-04-23 14:11:22 +0300

    add JSDOM

---
+++ src/tests/dom-default-document.test.ts
@@ -0,0 +1,24 @@
+
+
+describe('dom default document tests',()=>{
+    test('document is not null',()=>{
+        expect(document).not.toBeNull();
+    })
+
+    test('document.body.innerHTML with new div to be truthy',()=>{
+        document.body.innerHTML += '<div>123</div>'
+        expect(document.body.innerHTML).toBeTruthy();
+        expect(document.body.childNodes.length).toBe(1)
+    })
+
+    test('query selector is defined',()=>{
+        expect(document.querySelector).toBeDefined()
+    })
+
+    test('querySelector is not working by default',()=>{
+        document.body.innerHTML += '<div>123</div>'
+        expect(document.querySelectorAll('div')!.length).not.toBe(1)
+    })
+})
+
+export {} // to remove build error , can be removed after import
--- src/tests/dom.test.ts
+++ src/tests/dom.test.ts
@@ -1,13 +1,18 @@
+import jsdom from 'jsdom'
+const { JSDOM } = jsdom;
+
 describe('dom tests',()=>{
+    let document: any; 
+    beforeAll(()=>{
+        const html = '<!DOCTYPE html><body><p>Hello</p><p>world</p></body></html>'
+        document = (new JSDOM(html)).window.document;
+    })
+
     test('document is not null',()=>{
         expect(document).not.toBeNull();
     })
 
-    test('document.body.innerHTML with new div to be truthy',()=>{
-        document.body.innerHTML += '<div>123</div>'
-        expect(document.body.innerHTML).toBeTruthy();
-        expect(document.body.childNodes.length).toBe(1)
+    test('document has two p',()=>{
+        expect(document.querySelectorAll('p').length).toBe(2)
     })
 })
-
-export {} // just to remove build issue
